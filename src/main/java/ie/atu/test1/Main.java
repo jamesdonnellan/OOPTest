75c4682dcd251948d7f4287a2013e5063b633d27
JAMESDONNELLAN-STUDE <G00434671@atu.ie>
2024-11-06 17:38:46 +0000

    Added Changes to Master

--- src/main/java/ie/atu/test1/Main.java
+++ src/main/java/ie/atu/test1/Main.java
@@ -1,9 +1,13 @@
 package ie.atu.test1;
+import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Main
 {
     public static void main(String[] args)
     {
+        // Main Menu //
         System.out.println("WELCOME\n");
         System.out.println("Please enter command.\n");
         System.out.println("1. Add Product\n");
@@ -11,9 +15,58 @@ public class Main
         System.out.println("3. Display All Products\n");
         System.out.println("4. Exit\n");
 
-        Scanner p1 = new Scanner(System.in);
-        System.out.println("Enter Product Id: ");
-        String productId = p1.nextLine();
 
+        // Input validation //
+        String name = "Shirt";
+        String productId = "shirt123";
+        boolean IdChecker = false;
+        while(!IdChecker)
+        {
+            try
+            {
+                if (productId.equals(productId))
+                {
+                    System.out.println("Product found.");
+                    IdChecker = true;
+                    break;
+                }
+                else if (!IdChecker)
+                {
+                    System.out.println("Product not found. Invalid Input.");
+                }
+            }
+            catch (NumberFormatException ae)
+            {
+                System.out.println("Please try again");
+            }
+        }
+
+
+    // user selects an option
+
+    // if user enters add product
+        Scanner p1 = new Scanner(System.in);
+        System.out.println("Enter New Product Name: ");
+        String name = p1.nextline();
+       Product name = set.productId("");
+
+       // if user updates product
+
+        // if user displays all products
+        ArrayList<String> product = new ArrayList<>();
+        product.add("Shirts\n Product ID: shirt123\n Price: $10\n Quantity: 5");
+
+
+        System.out.println("Current Products: ");
+        for(int i=0; i < product.size(); i++)
+        {
+            System.out.println((i+1) + product.get(i));
+        }
+
+        // exits program
+    if(userInput = 4)
+        {
+            break;
+        }
     }
 }
